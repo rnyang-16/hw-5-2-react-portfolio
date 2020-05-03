ba8af63030c5d1bc3aa45bfa5241ce2c9042f17c
Ruonan Y <joyceyang0730@gmail.com>
2020-05-03 15:26:51 -0700

    add build

---
+++ build/precache-manifest.bc2b632357b6a17c22497049cf0ce395.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aee673111e6323799ee5cdf23f33e3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97104e4ffd0675afb860",
+    "url": "/static/js/2.c54c77e9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c54c77e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e4535fe8b9d4e6832f6",
+    "url": "/static/js/main.695ea5a0.chunk.js"
+  },
+  {
+    "revision": "38da0b020c5a6186febb",
+    "url": "/static/js/runtime-main.f49860f6.js"
+  }
+]);
